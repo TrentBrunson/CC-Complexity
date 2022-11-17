7464298685ae40004a7c28e333a0c7afedeed218
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2022-11-17 07:45:05 -0600

    EDA

--- complexity.py
+++ complexity.py
@@ -6,3 +6,50 @@ df = pd.read_excel('data\input\HC CC list - Offer Ops - Nov15.xlsx')
 # %%
 df
 # %%
+df.shape
+# %%
+df.info
+# %%
+df.describe()
+# %%
+df.columns
+# %%
+df.value_counts()
+# %%
+df.value_counts().sum()
+# %%
+df.count()
+# %%
+df.columns
+# %%
+# check for duplicate rows
+dupes = df.duplicated()
+sum(dupes)
+# %%
+# check for nulls
+# df.isnull - not useful
+
+df.isna().sum()
+# %%
+df['HC Cost Center'].unique()
+# %%
+NumUniqueCC = df['HC Cost Center'].nunique()
+NumUniqueCC
+# %%
+df.groupby(['HC Cost Center']).size()
+# %%
+df.groupby(['L6']).size()
+
+# %%
+df.groupby(['L6']).count()
+# %%
+test = df.groupby(['L6', 'HC Cost Center'])
+# %%
+test.first()
+# %%
+newDF = df[[
+    'L6',
+    'HC Cost Center'
+]]
+newDF
+# %%
